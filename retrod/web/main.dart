b00a5183037986bb1254303952a3566a4ceae9dc
Eric Taix <eric.taix@gmail.com>
2014-03-21 16:16:10 +0100

    Fix UI issues

--- retrod/web/main.dart
+++ retrod/web/main.dart
@@ -1,5 +1,6 @@
 import 'package:di/di.dart';
 import 'package:angular/angular.dart';
+import 'package:angular/playback/playback_http.dart';
 import 'retro.dart';
 
 import 'dart:html';
@@ -11,7 +12,31 @@ import 'dart:html';
 import 'dart:mirrors';
 
 main() {
-  print(window.location.search);
-  var module = new Module()
-      ..type(RetroController);
-}
+   print(window.location.search);
+   var module = new Module()
+       ..type(RetroController)
+       ..type(PlaybackHttpBackendConfig);
+ 
+   // If these is a query in the URL, use the server-backed
+   // TodoController.  Otherwise, use the stored-data controller.
+   var query = window.location.search;
+   if (query.contains('?')) {
+     module.type(ServerController, implementedBy: HttpServerController);
+   } else {
+     module.type(ServerController, implementedBy: NoServerController);
+   }
+ 
+   if (query == '?record') {
+     print('Using recording HttpBackend');
+     var wrapper = new HttpBackendWrapper(new HttpBackend());
+     module.value(HttpBackendWrapper, new HttpBackendWrapper(new HttpBackend()));
+     module.type(HttpBackend, implementedBy: RecordingHttpBackend);
+   }
+ 
+   if (query == '?playback') {
+     print('Using playback HttpBackend');
+     module.type(HttpBackend, implementedBy: PlaybackHttpBackend);
+   }
+ 
+   ngBootstrap(module: module);
+ }
